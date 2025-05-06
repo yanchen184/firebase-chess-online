1a5c4eff9bf9feb21eda7106345f4ba2a777db0f
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-06 09:47:12 +0800

    將 BrowserRouter 替換為 HashRouter 以修復 GitHub Pages 路由問題

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { GameProvider } from './contexts/GameContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <AuthProvider>
         <GameProvider>
           <App />
         </GameProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
